acc35741162adf906aa752b108edbc96ce763c13
kashitaka <makkausu@gmail.com>
2020-12-23 22:58:28 +0900

    finish 7.7

---
+++ chap7/exercise7.11/http.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if item == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "item required")
+		return
+	}
+	price, err := strconv.Atoi(req.URL.Query().Get("price"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "invalid price: %q\n", item)
+		return
+	}
+	db[item] = dollars(price)
+	fmt.Fprintf(w, "OK\n")
+}
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/update", db.update)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
---
+++ chap7/exercise7.12/http.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"text/template"
+)
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+const templ = `
+<html><body>
+<table border="1">
+<tr>
+<th>Item</th>
+<th>Price</th>
+</tr>
+{{range $key, $value := .}}
+<tr>
+<th>{{$key}}</th>
+<th>{{$value}}</th>
+</tr>
+{{end}}
+</table>
+</body></html>
+`
+
+var report = template.Must(template.New("db").
+	Parse(templ))
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	if err := report.Execute(w, db); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if item == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "item required")
+		return
+	}
+	price, err := strconv.Atoi(req.URL.Query().Get("price"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "invalid price: %q\n", item)
+		return
+	}
+	db[item] = dollars(price)
+	fmt.Fprintf(w, "OK\n")
+}
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/update", db.update)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
